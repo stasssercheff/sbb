ab5b4316b757c7154d1630f4ed3a75d9044a2d50
stasssercheff <stassserchef@gmail.com>
2026-03-16 11:26:02 +0700

    Update script.js

--- acc/script.js
+++ acc/script.js
@@ -7,8 +7,8 @@ const employeesRU = {
   "Женя": { position: "Су-шеф", rate: 800 },
   "Вадим": { position: "Повар", rate: 700 },
   "Hieu": { position: "Повар", rate: 600 },
-  "Денис": { position: "Повар", rate: 600 },
-  "Ирина": { position: "Кондитер", rate: 700 }
+  "Денис": { position: "Повар", rate: 700 },
+  "Ирина": { position: "Кондитер", rate: 750 }
 };
 
 
@@ -18,8 +18,8 @@ const employeesEN = {
   "Женя": { name: "Jenya", position: "Sous-chef", rate: 800 },
   "Вадим": { name: "Vadim", position: "Cook", rate: 700 },
   "Hieu": { name: "Hieu", position: "Cook", rate: 600 },
-  "Денис": { name: "Denis", position: "Cook", rate: 600 },
-  "Ирина": { name: "Irina", position: "Pastry", rate: 700 }
+  "Денис": { name: "Denis", position: "Cook", rate: 700 },
+  "Ирина": { name: "Irina", position: "Pastry", rate: 750 }
 };
 
 let csvData = [];
